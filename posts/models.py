45f5e37eca48b1c65184f0d28128b57952ab3415
mounikeshmintu <32958325+mounikeshmintu@users.noreply.github.com>
2019-06-29 13:52:10 +0530

    like post notification

--- posts/models.py
+++ posts/models.py
@@ -8,12 +8,16 @@ from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
 from django.contrib.auth.models import User
 from django.template.loader import render_to_string
+from django.conf import settings
+from django.db.models.signals import m2m_changed
+
 # from posts.models import Post
 
 # @receiver(post_delete,sender=Post)
 # @receiver(post_save,sender=Post)
 def change_post(sender, instance, *args, **kwargs):
 	post = Post.objects.all()
+
 	print('post is created')
 	html_users = render_to_string("includes/post.html",{'post':post})
 	channel_layer = get_channel_layer()
@@ -26,11 +30,20 @@ def change_post(sender, instance, *args, **kwargs):
 		}
 	)
 
+def like_post(sender, instance, *args, **kwargs):
+	results = Post.objects.filter(pk=49)
+	for users in results:
+		print(users.nc.all())
+	else:
+		print('no')
 
 # Create your models here.
-
 class Post(models.Model):
-    name=models.CharField(max_length=24)
-    def __str__(self):
-        return self.name
+
+	name=models.CharField(max_length=24)
+	nc=models.ManyToManyField(settings.AUTH_USER_MODEL)
+	def __str__(self):
+		return self.name
+    	# return self.name
+m2m_changed.connect(receiver=like_post,sender=Post.nc.through)
 signals.post_save.connect(receiver=change_post, sender=Post)
